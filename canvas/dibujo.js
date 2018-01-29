d16890a4e4c86b5de105d3b03114a72e8a4d43a7
Antonio <antoniobocanegragalindez@gmail.com>
2018-01-29 10:58:42 -0500

    while

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -1,16 +1,32 @@
 var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
 
+var lineas = 30;
+var l = 0;
+var yi, xf;
+
+while ( l < lineas) {
+
+    yi = 10 * l;
+    xf = 10 * (l + 1);
+    dibujarLinea("blue", 0, yi, xf, 300);
+    console.log("Linea "+ l);
+    l = l + 1;
+
+}
+
+dibujarLinea("green", 1, 1, 1, 299);
+dibujarLinea("green", 1, 299, 299, 299);
+
 function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal) {
 
     lienzo.beginPath();
     lienzo.strokeStyle = color;
-    lienzo.moveTo(xinicial, xinicial);
+    lienzo.moveTo(xinicial, yinicial);
     lienzo.lineTo(xfinal, yfinal);
     lienzo.stroke();
     lienzo.closePath();
 
 }
 
-dibujarLinea("red", 20, 200, 150, 50);
-dibujarLinea("blue", 150, 30, 20, 200);
+

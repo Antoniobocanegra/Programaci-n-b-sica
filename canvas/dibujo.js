14b98a51bbebb14a3f7255d302e5860165e52c1d
Antonio <antoniobocanegragalindez@gmail.com>
2018-01-29 12:15:23 -0500

    Eventos y formularios

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -1,21 +1,11 @@
+var texto = document.getElementById("texto_lineas")
+var boton = document.getElementById("botoncito")
+boton.addEventListener("click", dibujoPorClick);
+
 var d = document.getElementById("dibujito");
+var ancho = d.width;
 var lienzo = d.getContext("2d");
 
-var lineas = 30;
-var l = 0;
-var yi, xf;
-
-for( l; l < lineas; l++) {
-
-    yi = 10 * l;
-    xf = 10 * (l + 1);
-    dibujarLinea("blue", 0, yi, xf, 300);
-    console.log("Linea "+ l);
-
-}
-
-dibujarLinea("green", 1, 1, 1, 299);
-dibujarLinea("green", 1, 299, 299, 299);
 
 function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal) {
 
@@ -26,6 +16,27 @@ function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal) {
     lienzo.stroke();
     lienzo.closePath();
 
+}
+
+function dibujoPorClick() {
+
+    var lineas = parseInt(texto.value);
+    var l = 0;
+    var yi, xf;
+    var espacio = ancho /lineas;
+
+    for( l = 0; l < lineas; l++) {
+
+        yi = espacio * l;
+        xf = espacio * (l + 1);
+        dibujarLinea("blue", 0, yi, xf, 300);
+        console.log("Linea "+ l);
+
+    }
+
+    dibujarLinea("green", 1, 1, 1, 299);
+    dibujarLinea("green", 1, 299, 299, 299);
+    
 }
 
 
